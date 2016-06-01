489742f9b45de5de999c6b843de657df44f686fc
lchheng <lchheng@gmail.com>
2016-06-01 17:20:18 -0400

    forgot to include new class file

---
+++ includes/apple-exporter/components/class-fl-cove-video.php
@@ -0,0 +1,79 @@
+<?php
+namespace Apple_Exporter\Components;
+
+/**
+ * An HTML video tag representation.
+ *
+ * @since 0.2.0
+ */
+class FL_Cove_Video extends Component {
+
+	/**
+	 * Look for node matches for this component.
+	 *
+	 * @param DomNode $node
+	 * @return mixed
+	 * @static
+	 * @access public
+	 */
+	public static function node_matches( $node ) {
+		if (( 'div' == $node->nodeName ) && self::node_has_class( $node, 'fl-cove-video' )) {
+			return $node;
+		}
+		return null;
+	}
+
+	/**
+	 * Build the component.
+	 *
+	 * @param string $text
+	 * @access protected
+	 */
+	protected function build( $text ) {
+		// Remove initial and trailing tags: <video><p>...</p></video>
+		if ( ! preg_match( '/data-permalink="([^"]+)"/', $text, $match ) ) {
+			return null;
+		}
+
+		$url = $match[1];
+
+		$message = '[View this video on the original post](' . $url . ')';
+
+		$this->json = array(
+			'role' => 'title',
+			'text'   => $this->markdown->parse($message),
+			'format' => 'markdown',
+		);
+
+		$this->set_style();
+		$this->set_layout();
+	}
+	/**
+	 * Set the layout for the component.
+	 *
+	 * @access private
+	 */
+	private function set_layout() {
+		$this->json['layout'] = 'fl-cove-video-layout';
+		$this->register_layout( 'fl-cove-video-layout', array(
+			'margin' => array( 'top' => 5, 'bottom' => 5 ),
+		) );
+	}
+	/**
+	 * Set the style for the component.
+	 *
+	 * @access private
+	 */
+	private function set_style() {
+		$this->json[ 'textStyle' ] = 'fl-cove-video';
+		$this->register_style( 'fl-cove-video', array(
+			'fontName'      => $this->get_setting( 'pullquote_font' ),
+			'fontSize'      => 20,
+			'textColor'     => '#CF1515',
+			'textTransform' => $this->get_setting( 'pullquote_transform' ),
+			'lineHeight'    => intval( $this->get_setting( 'pullquote_line_height' ) ),
+			'textAlignment' => 'center',
+		) );
+	}
+}
+
